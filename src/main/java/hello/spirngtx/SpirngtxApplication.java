919402c8b9e8b45a595bf6216ffa890e6f5bef14
kimlee1031 <kwhazard0@gmail.com>
2023-10-25 14:13:09 +0900

    트랜젝션 확인

---
+++ src/main/java/hello/spirngtx/SpirngtxApplication.java
@@ -0,0 +1,13 @@
+package hello.spirngtx;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpirngtxApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpirngtxApplication.class, args);
+	}
+
+}
